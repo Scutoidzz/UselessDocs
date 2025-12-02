e1f4f8ff106b3335a7573f6d00d5c18c866752c4
Sterling Duncan <techedreal@gmail.com>
2025-12-02 16:11:15 -0600

    feat: Add 'Useless Mode' button, simplify share and save button logic to use alerts, and fix missing `</html>` tag in main.html.

--- logic.js
+++ logic.js
@@ -1,37 +1,11 @@
 const sharebutton = document.getElementById("sharebutton")
 const savebutton = document.getElementById("savebutton")
-let x = 0
-let y = 0
-let velocityX = 3
-let velocityY = 2
-const button = sharebutton
-function LocationCalculation() {
-    x = x + velocityX * 3
-    x = x - 4
-    y = y + velocityY * 3
-    y = y - 4
 
-}
-
-function FWya() {
-    const textarea = document.getElementById("long-textbox")
-    textarea.value = "WOBILLY WIGGLY WOBBLILT WIGGLY HUUHU";
-    textarea.style.color = "coral";
-
-    // Download the text.     
-
-
-
-}
-
-sharebutton.onclick = () => {
-    LocationCalculation()
-    sharebutton.style.left = x + 'px'
-    sharebutton.style.top = y + 'px'
-}
 
 savebutton.onclick = () => {
-    FWya()
+    window.alert("dooby dooby")
+};
 
-
-}
+sharebutton.onclick = async () => {
+    window.alert("DOOBY DOOBY!")
+};
